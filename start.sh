5e0c060a20788b0b122e7ffb3dbdb8ff71b231a3
michaelfarrell76 <mkfrl09@gmail.com>
2017-05-10 14:34:18 -0400

    updated start

--- start.sh
+++ start.sh
@@ -4,8 +4,20 @@ if command -v python3 > /dev/null 2>&1; then
 else
     echo Missing Python 3
     echo Downloading from https://www.continuum.io/downloads
+
+    if command -v wget > /dev/null 2>&1; then
+        echo wget installed
+    else
+      # Ensure brew exists
+      if command -v brew > /dev/null 2>&1; then
+          echo brew exists
+      else
+          /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      fi
+      brew install wget
+    fi
     wget https://repo.continuum.io/archive/Anaconda3-4.3.1-MacOSX-x86_64.sh
-    bash Anaconda3-4.3.1-MacOSX-x86_64.sh -y
+    bash Anaconda3-4.3.1-MacOSX-x86_64.sh -b
 fi
 
 # Ensure virtualenv exists
@@ -19,24 +31,6 @@ else
   fi
 fi
 
-
-# Ensure brew exists
-if command -v brew > /dev/null 2>&1; then
-    echo brew exists
-else
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-
-
-# Ensure ffmpeg exists
-if command -v ffmpeg > /dev/null 2>&1; then
-    echo ffmpeg exists
-else
-    brew install ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-frei0r --with-libass --with-libvo-aacenc --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-opus --with-rtmpdump --with-schroedinger --with-speex --with-theora --with-tools
-fi
-
-
 # check if virtual environment has been created before
 if [ ! -d "venv" ]; then
   virtualenv -p python3 venv
@@ -44,8 +38,21 @@ if [ ! -d "venv" ]; then
   source venv/bin/activate
   pip install -r requirements.txt
 else
-	# activate the environment
-	source venv/bin/activate
+  # activate the environment
+  source venv/bin/activate
+fi
+
+# Ensure ffmpeg exists
+if command -v ffmpeg > /dev/null 2>&1; then
+    echo ffmpeg exists
+else
+    # Ensure brew exists
+    if command -v brew > /dev/null 2>&1; then
+        echo brew exists
+    else
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    brew install ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-frei0r --with-libass --with-libvo-aacenc --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-opus --with-rtmpdump --with-schroedinger --with-speex --with-theora --with-tools
 fi
 
 # run the gui
